28f8e6525ea559b9297313038df7b81801705409
Andres Sainz <andresmsainz@gmail.com>
2021-04-26 23:13:45 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ComparisonOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Comparison Operators!");
+            int x = 0;
+            int y = 0;
+
+            Console.WriteLine("Enter No. 1");
+            x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter No. 2");
+            y = int.Parse(Console.ReadLine());
+
+            if (x == y)
+                Console.WriteLine(x + " is equal to " + y);
+            else
+                Console.WriteLine(x + " is not equal to " + y);
+            if (x > y)
+                Console.WriteLine(x + " is greater than " + y);
+            else if (x < y)
+                Console.WriteLine(x + " is less than " + y);
+        }
+    }
+}
