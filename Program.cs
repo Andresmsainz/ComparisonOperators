9b375d743056639b16872c1aae2edb013e244d7d
Andres Sainz <andresmsainz@gmail.com>
2021-04-26 23:47:51 -0400

    Added Logical Operators

--- Program.cs
+++ Program.cs
@@ -24,6 +24,96 @@ namespace ComparisonOperators
                 Console.WriteLine(x + " is greater than " + y);
             else if (x < y)
                 Console.WriteLine(x + " is less than " + y);
+
+            Console.WriteLine("Logical Operators!");
+
+            bool bHasLicence = false;
+            bool bKnowsHowToDrive = false;
+            string sResponse = "";
+
+            //Input
+            Console.WriteLine("Did you learn how to drive?");
+            sResponse = Console.ReadLine();
+
+            if (sResponse.ToLower() == "yes")
+            {
+                bKnowsHowToDrive = true;
+            }
+            else
+            {
+                bKnowsHowToDrive = false;
+            }
+
+            Console.WriteLine("Did you get your licence?");
+            sResponse = Console.ReadLine();
+
+            if (sResponse.ToLower() == "yes")
+            {
+                bHasLicence = true;
+            }
+            else
+            {
+                bHasLicence = false;
+            }
+
+            // Logic
+            if (bHasLicence == true && bKnowsHowToDrive == true)
+            {
+                Console.WriteLine("You are good to go");
+            }
+            else
+            {
+                if (bHasLicence == false && bKnowsHowToDrive == false)
+                {
+                    Console.WriteLine("You need to learn how to drive and get a licence");
+                }
+                else if (bKnowsHowToDrive == false)
+                {
+                    Console.WriteLine("You still need to learn how to drive");
+                }
+                else if (bHasLicence == false)
+                {
+                    Console.WriteLine("You still need to get your licence");
+                }
+                Console.WriteLine("The stars... not today");
+            }
+
+            bool bTookShower = false;
+            bool bWentForSwim = false;
+
+            //Input
+            Console.WriteLine("Did you take a shower?");
+            sResponse = Console.ReadLine();
+
+            if (sResponse.ToLower() == "yes")
+            {
+                bTookShower = true;
+            }
+            else
+            {
+                bTookShower = false;
+            }
+
+            Console.WriteLine("Did you go for a swim?");
+            sResponse = Console.ReadLine();
+
+            if (sResponse.ToLower() == "yes")
+            {
+                bWentForSwim = true;
+            }
+            else
+            {
+                bWentForSwim = false;
+            }
+
+            if (bWentForSwim || bTookShower)
+            {
+                Console.WriteLine("You are wet...");
+            }
+            else
+            {
+                Console.WriteLine("You are dry...");
+            }
         }
     }
 }
